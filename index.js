371d9709dbc650d6311680f4f82f414bd4ffb5d2
Aditya <adityakrjha111@gmai.com>
2023-09-18 22:48:49 +0530

    Upgraded to new version

--- index.js
+++ index.js
@@ -1,47 +1,107 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
-let player;
-let computer;
-let result;
+const stone = document.querySelector(".stone");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const reset = document.querySelector(".reset");
+const resultElement = document.querySelector(".result");
 
-choiceBtns.forEach(button => button.addEventListener("click", () => {
+const winsElement = document.querySelector(".wins");
+const lossesElement = document.querySelector(".losses");
+const tiesElement = document.querySelector(".ties");
+const youElement = document.querySelector(".you");
+const computerElement = document.querySelector(".comp");
 
-  player = button.textContent;
-  computerTurn();
-  playerText.textContent = `Player :  ${player}`;
-  computerText.textContent = `Computer :     ${computer}`;
-  resultText.textContent = checkWinner();
+let scoreBoard = JSON.parse(localStorage.getItem("score")) || {
+  Wins: 0,
+  Losses: 0,
+  Ties: 0
+};
 
-}))
+function computerMove() {
+  let random = Math.floor(Math.random() * 3) + 1;
+  let computer = "";
 
-function computerTurn() {
-  const randNum = Math.floor(Math.random() * 3) + 1;
-  switch (randNum) {
-    case 1:
-      computer = "ROCK";
-      break;
-    case 2:
-      computer = "PAPER";
-      break;
-    case 3:
-      computer = "SCISSOR";
-      break;
-  }
+  if (random === 1) computer = "Stone";
+  else if (random === 2) computer = "Paper";
+  else computer = "Scissor";
+
+  return computer;
 }
 
-function checkWinner(){
-  if(player == computer){
-    return "Draw!"; 
+function showScore(){
+  winsElement.innerHTML = `Wins: ${scoreBoard.Wins}`;
+  lossesElement.innerHTML = `Losses: ${scoreBoard.Losses}`;
+  tiesElement.innerHTML = `Ties: ${scoreBoard.Ties}`;
+}
+
+showScore();
+
+function playGame(playerMove) {
+  let computer = computerMove();
+  let result = "";
+
+  if (computer === "Stone") {
+    if (playerMove === "Paper") result = "You win";
+    else if (playerMove === "Scissor") result = "You lose";
+    else result = "Tie";
   } 
-  else if(computer == "ROCK"){
-    return (player == "PAPER") ? "You Win!" : "You Lose!"
+  else if (computer === "Paper") {
+    if (playerMove === "Stone") result = "You lose";
+    else if (playerMove === "Scissor") result = "You win";
+    else result = "Tie";
+  } 
+  else {
+    if (playerMove === "Stone") result = "You win";
+    else if (playerMove === "Paper") result = "You lose";
+    else result = "Tie";
   }
-  else if(computer == "PAPER"){
-    return (player == "SCISSOR") ? "You Win!" : "You Lose!"
-  }
-  else{
-    return (player == "ROCK") ? "You Win!" : "You Lose!"
-  }
+
+  //Update score board :
+  if (result === "You win") scoreBoard.Wins++;
+  else if (result === "You lose") scoreBoard.Losses++;
+  else scoreBoard.Ties++;
+
+  localStorage.setItem("score", JSON.stringify(scoreBoard)); //localStorage takes string to save data.
+
+  // Setting emoticons to player's move :
+  if (playerMove === "Stone") playerMove = "✊";
+  else if (playerMove === "Paper") playerMove = "🖐️";
+  else playerMove = "✌️";
+  // Setting emoticons to computer's move :
+  if (computer === "Stone") computer = "✊";
+  else if (computer === "Paper") computer = "🖐️";
+  else computer = "✌️";
+
+  // Showing result on screen:
+  resultElement.innerHTML = `${result}`;
+  youElement.innerHTML = `You : ${playerMove}`;
+  computerElement.innerHTML = `Computer : ${computer}`;
+
+  showScore();
 }
+
+stone.addEventListener("click", () => {
+  playGame("Stone");
+});
+
+paper.addEventListener("click", () => {
+  playGame("Paper");
+});
+
+scissor.addEventListener("click", () => {
+  playGame("Scissor");
+});
+
+reset.addEventListener("click", () => {
+  scoreBoard.Wins = 0;
+  scoreBoard.Losses = 0;
+  scoreBoard.Ties = 0;
+
+  // We need to remove the score from localStorage so that it resets the score in the localStorage after we click reset button. Otherwise it will just grab the previous values again. And we do not want that.
+
+  localStorage.removeItem("score");
+
+  resultElement.innerHTML = `Result`;
+  youElement.innerHTML = `You : ✊`;
+  computerElement.innerHTML = `Computer : ✊`;
+  showScore();
+});
